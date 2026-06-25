7bad249a76faff6c30951f3387d8faf0146b9133
Sujata Birajdar <sujatabirajdar@yahoo.in>
2026-06-25 17:03:18 +0530

    added pom.xml and src folders

---
+++ src/main/java/com/example/app.java
@@ -0,0 +1,11 @@
+ackage com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(getGreeting());
+    }
+
+    public static String getGreeting() {
+        return "Hello, GitHub Actions!";
+    }
+}
